4f435060b69e0e3c5d005a82982a408d9d8391c5
james58899 <james59988@gmail.com>
2020-02-27 14:07:25 +0800

    Add system DNS as fallback

--- app/src/main/java/one/oktw/muzeipixivsource/util/AppUtil.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/util/AppUtil.kt
@@ -4,39 +4,44 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.LinkProperties
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AlertDialog
 import androidx.core.net.toUri
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.ConnectionSpec
+import okhttp3.Dns
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.dnsoverhttps.DnsOverHttps
 import okhttp3.internal.platform.Platform
 import one.oktw.muzeipixivsource.R
 import one.oktw.muzeipixivsource.hack.DisableSNISSLSocketFactory
+import one.oktw.muzeipixivsource.hack.FallbackDns
 import java.net.InetAddress
 
 class AppUtil {
     companion object {
         const val MUZEI_PACKAGE = "net.nurik.roman.muzei"
+        private val bootstrapDNS = arrayListOf<InetAddress>().apply {
+            addAll(listOf("104.16.248.249", "104.16.249.249").map(InetAddress::getByName))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) addAll(LinkProperties().dnsServers)
+        }
+        private val dns = FallbackDns(DnsOverHttps.Builder()
+            .client(OkHttpClient())
+            .url("https://cloudflare-dns.com/dns-query".toHttpUrl())
+            .post(true)
+            .bootstrapDnsHosts(bootstrapDNS)
+            .build(), Dns.SYSTEM)
         val GSON: Gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
         val httpClient = OkHttpClient().newBuilder()
+            .retryOnConnectionFailure(true)
             .connectionSpecs(listOf(ConnectionSpec.Builder(ConnectionSpec.RESTRICTED_TLS).supportsTlsExtensions(false).build()))
             .sslSocketFactory(DisableSNISSLSocketFactory(), Platform.get().platformTrustManager())
-            .dns(
-                DnsOverHttps.Builder()
-                    .client(OkHttpClient())
-                    .url("https://cloudflare-dns.com/dns-query".toHttpUrl())
-                    .post(true)
-                    .bootstrapDnsHosts(
-                        InetAddress.getByName("104.16.249.249"),
-                        InetAddress.getByName("104.16.248.249")
-                    )
-                    .build()
-            )
+            .dns(dns)
             .build()
 
         fun checkInstalled(context: Context, packageName: String): Boolean {
