3ee0b2e03ad782c1e1e93219d8f53c172f3d43ca
james58899 <james59988@gmail.com>
2020-03-06 16:35:10 +0800

    Add Google DNS

--- app/src/main/java/one/oktw/muzeipixivsource/util/HttpUtils.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/util/HttpUtils.kt
@@ -1,7 +1,5 @@
 package one.oktw.muzeipixivsource.util
 
-import android.net.LinkProperties
-import android.os.Build
 import okhttp3.ConnectionSpec
 import okhttp3.Dns
 import okhttp3.HttpUrl.Companion.toHttpUrl
@@ -21,17 +19,23 @@ object HttpUtils {
         Security.insertProviderAt(Conscrypt.newProvider(), 1)
     }
 
-    private val bootstrapDNS = arrayListOf<InetAddress>().apply {
-        addAll(listOf("104.16.248.249", "104.16.249.249").map(InetAddress::getByName))
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) addAll(LinkProperties().dnsServers)
-    }
-    private val dns = FallbackDns(DnsOverHttps.Builder()
+    private val ipv6Ready = NetworkInterface.getNetworkInterfaces().toList()
+        .any { it.interfaceAddresses.any { i -> i.address.run { this is Inet6Address && !isLoopbackAddress && !isLinkLocalAddress && !isSiteLocalAddress } } }
+    private val cloudFlareDoH = DnsOverHttps.Builder()
         .client(OkHttpClient())
-        .url("https://cloudflare-dns.com/dns-query".toHttpUrl())
+        .url("https://muzeipixivsource.cloudflare-dns.com/dns-query".toHttpUrl())
         .post(true)
-        .includeIPv6(NetworkInterface.getNetworkInterfaces().toList().any { it.interfaceAddresses.any { it.address.run { this is Inet6Address && !isLoopbackAddress && !isLinkLocalAddress && !isSiteLocalAddress } } })
-        .bootstrapDnsHosts(bootstrapDNS)
-        .build(), Dns.SYSTEM)
+        .includeIPv6(ipv6Ready)
+        .bootstrapDnsHosts(listOf("104.16.248.249", "104.16.249.249").map(InetAddress::getByName))
+        .build()
+    private val googleDoH = DnsOverHttps.Builder()
+        .client(OkHttpClient())
+        .url("https://dns.google/dns-query".toHttpUrl())
+        .post(true)
+        .includeIPv6(ipv6Ready)
+        .bootstrapDnsHosts(listOf("8.8.8.8", "8.8.4.4").map(InetAddress::getByName))
+        .build()
+    private val dns = FallbackDns(cloudFlareDoH, googleDoH, Dns.SYSTEM)
 
     val httpClient = OkHttpClient().newBuilder()
         .retryOnConnectionFailure(true)
