6dadb5a8f830ddb174ed33682400f507a77f57b1
james58899 <james59988@gmail.com>
2020-09-24 13:42:50 +0800

    Show error message on login fail

--- app/src/main/java/one/oktw/muzeipixivsource/activity/PixivSignIn.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/activity/PixivSignIn.kt
@@ -91,7 +91,7 @@ class PixivSignIn : AppCompatActivity(), CoroutineScope by CoroutineScope(Dispat
                 login_button.isEnabled = true
 
                 // TODO show error message
-                Snackbar.make(login_layout, R.string.login_fail, Snackbar.LENGTH_LONG).show()
+                Snackbar.make(login_layout, getString(R.string.login_fail) + "\n${login.errors?.system?.message}", Snackbar.LENGTH_LONG).show()
             }
         }
     }
