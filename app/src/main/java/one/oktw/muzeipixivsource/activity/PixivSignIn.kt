904ea8c8aa0fa7747559df891faf24e37c7f4755
James58899 <james59988@gmail.com>
2021-02-24 15:13:52 +0800

    OAuth system webview version

--- app/src/main/java/one/oktw/muzeipixivsource/activity/PixivSignIn.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/activity/PixivSignIn.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.preference.PreferenceManager
 import kotlinx.coroutines.*
 import one.oktw.muzeipixivsource.R
 import one.oktw.muzeipixivsource.pixiv.PixivOAuth
-import org.mozilla.geckoview.*
 import java.security.MessageDigest
 import java.security.SecureRandom
 
@@ -22,57 +23,56 @@ class PixivSignIn : AppCompatActivity(), CoroutineScope by CoroutineScope(Dispat
     private val messageDigest = MessageDigest.getInstance("SHA-256")
     private val securityRandom = SecureRandom()
     private lateinit var code: String
+    private lateinit var webView: WebView
+    private var bypassDomainCheck = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Set up the login form.
         setContentView(R.layout.activity_pixiv_login)
-        val view: GeckoView = findViewById(R.id.geckoview)
-        val session = GeckoSession()
-        val runtime = GeckoRuntime.getDefault(this)
-        session.navigationDelegate = object : GeckoSession.NavigationDelegate {
-            override fun onLoadRequest(session: GeckoSession, request: GeckoSession.NavigationDelegate.LoadRequest): GeckoResult<AllowOrDeny>? {
-                if (request.isRedirect && request.uri.startsWith("pixiv://")) {
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.userAgentString = webView.settings.userAgentString.replace(Regex("Version/\\d\\.\\d\\s"), "") // Hide WebView version
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                if (request.isRedirect && request.url.scheme == "pixiv") {
                     // Async handle
                     launch(Dispatchers.IO) {
-                        request.uri.toUri().getQueryParameter("code")?.let {
-                            PixivOAuth.login(code, it).also { Log.i("LOGIN", it.toString()) }
+                        request.url.getQueryParameter("code")?.let {
+                            PixivOAuth.login(code, it)
                         }?.let {
                             if (!it.has_error && it.response != null)
                                 withContext(Dispatchers.Main) {
                                     PixivOAuth.save(PreferenceManager.getDefaultSharedPreferences(this@PixivSignIn), it.response)
-                                    session.close()
+                                    webView.destroy()
                                     setResult(RESULT_OK)
                                     finish()
                                 }
                         } ?: withContext(Dispatchers.Main) {
                             // TODO error message
-                            session.close()
                             finish()
                         }
                     }
 
-                    return GeckoResult.DENY
+                    return true
                 }
 
                 // Disallow user use WebView browser other page
-                if (request.uri.toUri().host !in allowDomain && !request.uri.startsWith("https://www.pixiv.net/logout.php")) {
-                    launch {
-                        startActivity(Intent(Intent.ACTION_VIEW, request.uri.toUri()))
+                if (!bypassDomainCheck && request.url.host !in allowDomain && !request.url.toString().startsWith("https://www.pixiv.net/logout.php")) {
+                    if (request.url.host == "socialize.gigya.com") bypassDomainCheck = true else {
+                        startActivity(Intent(Intent.ACTION_VIEW, request.url))
+                        return true
                     }
-                    return GeckoResult.DENY
                 }
-                return super.onLoadRequest(session, request)
+                return false
             }
         }
 
-        session.open(runtime)
-        view.setSession(session)
         val (code, hash) = generateCodeAndHash()
         this.code = code
         Log.i("LOGIN webview", "code: $code, hash: $hash")
-        session.loadUri("https://app-api.pixiv.net/web/v1/login?code_challenge=$hash&code_challenge_method=S256&client=pixiv-android") // Or any other URL...
+        webView.loadUrl("https://app-api.pixiv.net/web/v1/login?code_challenge=$hash&code_challenge_method=S256&client=pixiv-android") // Or any other URL...
     }
 
     private fun generateCodeAndHash(): Pair<String, String> {
