f0ae2f2240194afe8c5a74e0d80902fb24b272dc
James58899 <james59988@gmail.com>
2021-06-07 14:29:15 +0800

    Impl MuzeiDocumentProvider

---
+++ app/src/main/java/one/oktw/muzeipixivsource/provider/MuzeiDocumentProvider.kt
@@ -0,0 +1,5 @@
+package one.oktw.muzeipixivsource.provider
+
+import com.google.android.apps.muzei.api.provider.MuzeiArtDocumentsProvider
+
+class MuzeiDocumentProvider : MuzeiArtDocumentsProvider()
