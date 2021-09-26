f83f0c0552cbfa81802956feadbc2d2f74a39f79
James58899 <james59988@gmail.com>
2021-09-26 15:20:45 +0800

    Clean up

--- app/src/main/java/one/oktw/muzeipixivsource/pixiv/model/APIError.kt
+++
@@ -1,7 +0,0 @@
-package one.oktw.muzeipixivsource.pixiv.model
-
-data class APIError(
-    val userMessage: String,
-    val message: String,
-    val reason: String
-)
--- app/src/main/java/one/oktw/muzeipixivsource/provider/Commands.kt
+++ app/src/main/java/one/oktw/muzeipixivsource/provider/Commands.kt
@@ -1,7 +1,7 @@
 package one.oktw.muzeipixivsource.provider
 
-enum class Commands(val code: Int) {
-    COMMAND_OPEN(2),
-    COMMAND_SHARE(3),
-    COMMAND_DOWNLOAD(4),
+enum class Commands {
+    COMMAND_OPEN,
+    COMMAND_SHARE,
+    COMMAND_DOWNLOAD,
 }
